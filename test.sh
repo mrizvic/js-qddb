46ebbdebd6dd1ac818b733c90121a58878b5b5e3
mrizvic <marko.rizvic@gmail.com>
2015-07-06 10:47:34 +0200

    minor change

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-curl -L http://127.0.0.1:8002/key1/value1 -XPUT
-curl -L http://127.0.0.1:8002/key2/2 -XPUT
-curl -L http://127.0.0.1:8002/3/3 -XPUT
-curl -L http://127.0.0.1:8002/temperature/23.45 -XPUT
-curl -L http://127.0.0.1:8002/humidity/56.78 -XPUT
-curl -L http://127.0.0.1:8002/cmd/list
+curl http://127.0.0.1:8002/key1/value1 -XPUT
+curl http://127.0.0.1:8002/key2/2 -XPUT
+curl http://127.0.0.1:8002/3/3 -XPUT
+curl http://127.0.0.1:8002/temperature/23.45 -XPUT
+curl http://127.0.0.1:8002/humidity/56.78 -XPUT
+curl http://127.0.0.1:8002/cmd/list
