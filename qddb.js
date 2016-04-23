38ebb482703552b860fe1535f576f078bb26ac7f
mrizvic <marko.rizvic@gmail.com>
2016-04-23 12:59:34 +0200

    implemented FIFO storage for key/variables

--- qddb.js
+++ qddb.js
@@ -1,12 +1,19 @@
-// TODO (always) write more error handling stuff
+// IMPORTANT: run with nodejs --expose-gc to ensure memory optimisation
+
 var host = '127.0.0.1';
 var port = 8002;
-var myAppName="qddb.js";	//quick and dirty database
+var myAppName=__filename;
+var myURL = 'http://' + host + ':' + port;
 
 require("console-stamp")(console, "yyyy-mm-dd HH:MM:ss.l");
-
 console.log('PID: ' + process.pid)
 console.log('Application name: ' + myAppName)
+
+if (global.gc) {
+	console.log('global.gc() function enabled');
+} else {
+	console.log('global.gc() function disabled');
+}
 
 var sizeof = require('object-sizeof');
 var express = require('express');
@@ -15,7 +22,7 @@ var app = express();
 // Add headers
 app.use(function (req, res, next) {
 	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'http://io.dmz6.net');
+	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
 
 	// Request methods you wish to allow
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -38,117 +45,204 @@ var counterPutPost=0;
 //var counterPost=0;
 var counterDelete=0;
 
+var dynamicKeys=0;
+var valueCounter=0;
 
-// objects
-var KVObject = function(key, value) {
-	this.key = key
-	this.value = value
-	this.modified = new Date()
-	this.created = new Date()
-	this.accessed = new Date()
-}
+// DYNAMIC OBJECTS START
+// key/value objects
+var KVObject_dynamic = function(key) {
+    this.key = key;
+	this.value = undefined;
+    this.values = Array();
+    this.created = new Date();
+    this.accessed = undefined;
+    this.modified = undefined;
+};
 
+// storage for key/value objects
+var arrayKVObjects_dynamic = {};
 
-// object storage
-var arrayKVObjects = [];
+// DYNAMIC OBJECTS STOP
 
 // store object procedure
-storeKeyValue = function(req,res) {
+storeKV_dynamic = function(req,res) {
 	apiRequests++;
 	counterPutPost++;
-	console.log(req.params);
+	console.log('POST / ' + JSON.stringify(req.params));
 	var key = req.params.key;
 	var value = req.params.value;
+	var limit = req.params.limit;
 
-	exists = typeof(arrayKVObjects[key]);
-
-	if (exists == 'undefined') {
-		console.log('doesnt exist, creating new object, key='+key)
-		arrayKVObjects[key] = new KVObject(key, value);
-	} else {
-		arrayKVObjects[key].value = value;
-		arrayKVObjects[key].modified = new Date();
-	}
+	raw_storeKV_dynamic(key, value, limit);
 
 	res.send('OK');
 };
 
+// store object procedure for performance testing
+raw_storeKV_dynamic = function(key, value, limit) {
+    var exists = typeof(arrayKVObjects_dynamic[key]);
 
-// get all objects
+	// if key undefiend then create new entry
+    if (exists == 'undefined') {
+        console.log('dynamic object doesnt exist, creating new object, key='+key)
+        arrayKVObjects_dynamic[key] = new KVObject_dynamic(key);
+        dynamicKeys++;
+    }
+
+	var myDate = new Date();
+	var tmp = [myDate, value]
+	// if limit undefined store single value, else push [timestamp,value] to array
+	if (typeof(limit) == 'undefined') {
+		arrayKVObjects_dynamic[key].values=[tmp];
+	} else {
+    	arrayKVObjects_dynamic[key].values.push(tmp);
+		var valueCount = arrayKVObjects_dynamic[key].values.length;
+		// splice table if it exceeds limit
+    	if (valueCount > limit) {
+			howMany = valueCount - limit;
+       	 	arrayKVObjects_dynamic[key].values.splice(0,howMany);
+    	}
+    }
+	arrayKVObjects_dynamic[key].modified = myDate;
+	valueCounter = arrayKVObjects_dynamic[key].values.length;
+};
+
+// get all dynamic objects
 app.get('/cmd/list', function(req, res) {
 	apiRequests++;
+	counterGet++;
 	var keys = [];
-	for (var key in arrayKVObjects) {
-		keys.push(arrayKVObjects[key]);
+	for (var key in arrayKVObjects_dynamic) {
+		keys.push(arrayKVObjects_dynamic[key]);
 	}
 	res.send(keys);
 });
 
 
-// report for android Universal Widget
-// https://play.google.com/store/apps/details?id=uk.cdev.universalwidget.v1&hl=en
-app.get('/cmd/uw', function(req,res) {
-	var myresp = {};
-	myresp['title'] = 'my internet of things';
-	myresp['type'] = 'list';
-	myresp['date'] = new Date().toString();
-
-	var data = [];
-	for (var key in arrayKVObjects) {
-		var mytemp = {};
-		var myvalue = arrayKVObjects[key].value;
-		// skip long lines
-		if (myvalue.length > 15) {
-			continue;
-		}
-		mytemp['name'] = key;
-		mytemp['value'] = myvalue;
-		arrayKVObjects[key].accessed = new Date();
-		//mytemp['color'] = 'yellow';
-		data.push(mytemp);
-	}
-	myresp['data'] = data;
-	res.send(myresp);
-});
-
-// get value for specific key
+// get info and single value for specific dynamic object
 app.get('/:key', function(req,res) {
 	apiRequests++;
 	counterGet++;
-	console.log(req.params);
+	console.log('GET / ' + JSON.stringify(req.params));
 	var key = req.params.key;
-	exists = typeof(arrayKVObjects[key]);
+	var myDate = new Date();
+	var resp = {};
+	exists = typeof(arrayKVObjects_dynamic[key]);
 
+	// if key doesnt exist produce error message
 	if (exists == 'undefined') {
-		var resp = {};
-		resp['error'] = 'undefined';
-		console.log(JSON.stringify(resp));
-		res.send(resp);
+		res.status(404).send('nonexistent object');
+		return;
 	} else {
-		var resp = {};
+		var myLength = arrayKVObjects_dynamic[key].values.length;
 		resp['key']=key;
-		resp['value']=arrayKVObjects[key].value;
-		resp['accessed']=arrayKVObjects[key].accessed;
-		res.send(resp);
-		arrayKVObjects[key].accessed = new Date();
+		// if more than one stored value
+		if (myLength > 1) {
+			resp['first_timestamp']=arrayKVObjects_dynamic[key].values.slice(0,1)[0][0];
+			resp['last_timestamp']=arrayKVObjects_dynamic[key].values.slice(-1)[0][0];
+			resp['first_value']=arrayKVObjects_dynamic[key].values.slice(0,1)[0][1];
+			resp['last_value']=arrayKVObjects_dynamic[key].values.slice(-1)[0][1];
+		}
+		// return last value as 'value'
+		resp['value']=arrayKVObjects_dynamic[key].values.slice(-1)[0][1];
+		resp['accessed']=arrayKVObjects_dynamic[key].accessed;
+		resp['created']=arrayKVObjects_dynamic[key].created;
+		resp['modified']=arrayKVObjects_dynamic[key].modified;
+		resp['total_values']=myLength;
+		arrayKVObjects_dynamic[key].accessed = myDate;
 	}
+	res.send(resp);
 });
 
-// delete specific key
+// get last ':limit' values for specific dynamic object
+app.get('/:key/:limit(\\d+)', function(req,res) {
+	apiRequests++;
+	counterGet++;
+	console.log('GET / ' + JSON.stringify(req.params));
+	var key = req.params.key;
+	var limit = req.params.limit;
+	var myDate = new Date();
+	var resp = {};
+	exists = typeof(arrayKVObjects_dynamic[key]);
+
+	if (exists == 'undefined') {
+		res.status(404).send('nonexistent object');
+		return;
+	} else if (limit > myLength) {
+		var myLength = arrayKVObjects_dynamic[key].values.length;
+		// produce warning and limit response to myLength
+		resp['warning'] = 'limit exceeds number of stored elements: ' + myLength + '/' + limit;
+		console.log(JSON.stringify(resp));
+		limit = myLength;
+		//res.send(resp);
+	}
+	resp['key']=key;
+	resp['values']=arrayKVObjects_dynamic[key].values.slice(-limit);
+	resp['accessed']=arrayKVObjects_dynamic[key].accessed;
+	resp['created']=arrayKVObjects_dynamic[key].created;
+	resp['modified']=arrayKVObjects_dynamic[key].modified;
+	arrayKVObjects_dynamic[key].accessed = myDate;
+	res.send(resp);
+	
+});
+
+// get values for specific dynamic object stored between ':from' and ':to' timestamp
+app.get('/:key/:from(\\d+)/:to(\\d+)', function(req,res) {
+	apiRequests++;
+	counterGet++;
+	console.log('GET / ' + JSON.stringify(req.params));
+	var key = req.params.key;
+	var from = req.params.from;
+	var to = req.params.to;
+	var myDate = new Date();
+	var resp = {};
+	from = new Date(from*1000);
+	to = new Date(to*1000);
+	exists = typeof(arrayKVObjects_dynamic[key]);
+
+	if (exists == 'undefined') {
+		res.status(404).send('nonexistent object');
+		return;
+	} else {
+		var myLength = arrayKVObjects_dynamic[key].values.length;
+		resp['key']=key;
+		resp['values'] = Array();
+		// loop through all keys and match to >= keytimestamp >= from
+		for (i=0; i<myLength; i++) {
+			myTS = arrayKVObjects_dynamic[key].values[i][0];
+			if ( (myTS >= from) && (myTS <= to) ) {
+				myValue = arrayKVObjects_dynamic[key].values[i][1];
+				resp['values'].push([myTS,myValue]);
+			}
+		}
+		resp['accessed']=arrayKVObjects_dynamic[key].accessed;
+		resp['created']=arrayKVObjects_dynamic[key].created;
+		resp['modified']=arrayKVObjects_dynamic[key].modified;
+		arrayKVObjects_dynamic[key].accessed = myDate;
+	}
+	res.send(resp);
+});
+
+// delete dynamic object
 app.delete('/:key', function(req,res) {
 	apiRequests++;
 	counterDelete++;
-	console.log(req.params);
+	console.log('DELETE /' + JSON.stringify(req.params));
 	var key = req.params.key;
-	delete arrayKVObjects[key];
+	arrayKVObjects_dynamic[key] = null;
+	delete arrayKVObjects_dynamic[key];
+	if (global.gc) {
+		global.gc();
+	}
 	res.send('OK');
 });
 
 // methods to store key/value object
-app.put('/:key/:value', storeKeyValue);
-app.post('/:key/:value', storeKeyValue);
+app.put('/:key/:value', storeKV_dynamic);
+app.post('/:key/:value', storeKV_dynamic);
 
-
+app.put('/:key/:value/:limit', storeKV_dynamic);
+app.post('/:key/:value/:limit', storeKV_dynamic);
 
 // get some stats
 var printStatistics = function() {
@@ -156,8 +250,11 @@ var printStatistics = function() {
 	console.info('GET requests: ' + counterGet);
 	console.info('PUT/POST requests: ' + counterPutPost);
 	console.info('DELETE requests: ' + counterDelete);
-	console.info('objects in memory: ' + Object.keys(arrayKVObjects).length);
-	console.info('objects memory usage: ' + sizeof(arrayKVObjects));
+	// console.info('static keys in memory: ' + Object.keys(arrayKVObjects).length);
+	console.info('dynamic keys in memory: ' + dynamicKeys);
+	console.info('dynamic values in memory: ' + valueCounter);
+	// console.info('array memory usage: ' + (sizeof(arrayKVObjects) + sizeof(arrayKVObjects_dynamic)));
+	console.info('array memory usage: ' + sizeof(arrayKVObjects_dynamic));
 };
 
 var signalHandler = function() {
@@ -199,4 +296,74 @@ process.on('uncaughtException', function(err) {
 	} else {
 		console.error(err);	
 	}
-})
+});
+
+// var stdin = process.openStdin();
+// stdin.addListener('data', function(dataz) {
+//         //console.log('echo back: ' + dataz);
+//         if (dataz.toString().trim() == 'stats') {
+//                 printStatistics();
+//         }
+// });
+
+// uncomment this for performance and memory test
+//var perftest=1;
+
+if (typeof(perftest) != 'undefined') {
+	console.log('raw_storeKV_dynamic start');
+	// for (var i=10000000000; i<10000015000; i++) {
+	for (var i=1; i<10; i++) {
+		raw_storeKV_dynamic('key1',i,15000)
+		// raw_storeKV_dynamic('nucukwudisjajEuphmov',i,15000)
+		// raw_storeKV_dynamic('civCeneevyehalhuvtho',i,15000)
+		// raw_storeKV_dynamic('Eshkyifyunbodganfech',i,15000)
+		// raw_storeKV_dynamic('Moz5grumcaxHuiltugUd',i,15000)
+		// raw_storeKV_dynamic('TePetbecsyecWefNiabu',i,15000)
+		// raw_storeKV_dynamic('rackgenmenIjNuwobres',i,15000)
+		// raw_storeKV_dynamic('egWerlOrOacVakCegJer',i,15000)
+		// raw_storeKV_dynamic('DouqueOpOorUnlewkom3',i,15000)
+		// raw_storeKV_dynamic('JeishOrodEfOomDetEnk',i,15000)
+		// raw_storeKV_dynamic('caywyFlydLonocOlCerv',i,15000)
+		// raw_storeKV_dynamic('VuswapNevUndoddIbceo',i,15000)
+		// raw_storeKV_dynamic('RawbijoppAnPoucosheb',i,15000)
+		// raw_storeKV_dynamic('yuspOdyivoughUtshEm5',i,15000)
+		// raw_storeKV_dynamic('ivDaquochCidjaHeunri',i,15000)
+		// raw_storeKV_dynamic('cricirbibyimtOdVilhi',i,15000)
+		// raw_storeKV_dynamic('vikwesEpvabnawumtUji',i,15000)
+		// raw_storeKV_dynamic('Did7ShraynajaldOpBia',i,15000)
+		// raw_storeKV_dynamic('VuIjBeunLapcushDick6',i,15000)
+		// raw_storeKV_dynamic('pocdueckDatyigBuvAsy',i,15000)
+		// raw_storeKV_dynamic('LoysgapMyboytseehiJu',i,15000)
+		// raw_storeKV_dynamic('nesOvruhakCyrenGherg',i,15000)
+		// raw_storeKV_dynamic('CatOtnexCeth7FroazOg',i,15000)
+		// raw_storeKV_dynamic('IlumibsUsackImIrdyom',i,15000)
+		// raw_storeKV_dynamic('JeirulretbiafdinIdKo',i,15000)
+		// raw_storeKV_dynamic('RilibrAccutEynAdpiep',i,15000)
+		// raw_storeKV_dynamic('krosyetDynckyeshvoys',i,15000)
+		// raw_storeKV_dynamic('MidhedUrpiSlaipeejDo',i,15000)
+		// raw_storeKV_dynamic('geegoufavtiemuQuigci',i,15000)
+		// raw_storeKV_dynamic('SanmymfehuIbcapdeika',i,15000)
+		// raw_storeKV_dynamic('OyptoglaxEzHuridokcu',i,15000)
+		// raw_storeKV_dynamic('wavUgvifolfEurfAtIby',i,15000)
+		// raw_storeKV_dynamic('FaiTworyijPhicIdJesp',i,15000)
+		// raw_storeKV_dynamic('GaitjottyesjoBloicyi',i,15000)
+		// raw_storeKV_dynamic('4OvEwHivRewOmticmek0',i,15000)
+		// raw_storeKV_dynamic('ocGarAtkeptacDeshbon',i,15000)
+		// raw_storeKV_dynamic('guhywofMenshijvufMaw',i,15000)
+		// raw_storeKV_dynamic('UpDeOkjikeemodyivDit',i,15000)
+		// raw_storeKV_dynamic('swadwyfuenjofAfcyWed',i,15000)
+		// raw_storeKV_dynamic('blyujShokumauHyojkuc',i,15000)
+		// raw_storeKV_dynamic('yolHyanwesvemcigudKi',i,15000)
+		// raw_storeKV_dynamic('CrigFesvadCawcicjuvu',i,15000)
+		// raw_storeKV_dynamic('TidPefyabeepGogfigDa',i,15000)
+		// raw_storeKV_dynamic('cheymWoshFeneavjumMa',i,15000)
+		// raw_storeKV_dynamic('AydjipsejDisyucullud',i,15000)
+		// raw_storeKV_dynamic('FliowItOldyeytijlerv',i,15000)
+		// raw_storeKV_dynamic('isvizeashuchotisBov2',i,15000)
+		// raw_storeKV_dynamic('DyriwejpiriOntipWeal',i,15000)
+		// raw_storeKV_dynamic('niWyickedJiognobKogs',i,15000)
+		// raw_storeKV_dynamic('codBushkiccypNafcyni',i,15000)
+		// raw_storeKV_dynamic('Adrynhefefvostidadtu',i,15000)
+	}
+	console.log('raw_storeKV_dynamic stop');
+}
